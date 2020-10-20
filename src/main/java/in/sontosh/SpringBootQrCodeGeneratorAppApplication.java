0638a29401f65ffdda40a13f660f72a62022e0a5
rohitpm <balkrishnalenka@gmail.com>
2020-10-20 19:16:13 +0530

    qr-20-10-2020

---
+++ src/main/java/in/sontosh/SpringBootQrCodeGeneratorAppApplication.java
@@ -0,0 +1,13 @@
+package in.sontosh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootQrCodeGeneratorAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootQrCodeGeneratorAppApplication.class, args);
+	}
+
+}
